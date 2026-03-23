274bbef277dfe51e896a263be6da28977ad0cbe7
alok kumar priyadarshi <155714900+Alok-kumar-priyadarshi@users.noreply.github.com>
2026-03-23 21:26:09 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export const sendMessage = async ({ query, url, file, session_id }) => {
